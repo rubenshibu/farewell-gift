3bb052837a335204aa2e4c9db717dac8dee2fc52
tonalmathew <tonalmathew1999@gmail.com>
2021-07-09 23:06:37 +0530

    feat: activate js

--- js/display.image.js
+++ js/display.image.js
@@ -1,25 +1,35 @@
 $(document).ready(function () {
   let image = [
     {
-      imgUrl: "Tech Talk - 2",
+      imgUrl: "../images/img_1.jpg",
+    },
+    {
+      imgUrl: "../images/img_2.jpg",
+    },
+    {
+      imgUrl: "../images/img_3.jpg",
+    },
+    {
+      imgUrl: "../images/img_4.jpg",
+    },
+    {
+      imgUrl: "../images/img_5.jpg",
     },
   ];
 
-  events.forEach((event) => {
-    $(".event-cards")
-      .append(`<div class="card border-dark mb-3 rounded event-card" >
-    <div class="row no-gutters">
-      <div class="col-md-4">
-      <a class="venobox" href="${event.image}"><img src="${event.image}" alt="image alt" class="card-img card-img-class "/></a>
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">${event.eventName}</h5>
-          <p class="card-text my-0"><small class="text-muted">${event.eventDate} </small></p>
-          <p class="card-text">${event.report}</p>
-        </div>
-      </div>
-    </div>
-  </div>`);
+  image.forEach((img) => {
+    $(".images")
+      .append(`<div class="col-6 col-md-6 col-lg-3" data-aos="fade-up">
+                <a
+                  href="images/img_1.jpg"
+                  class="d-block photo-item"
+                  data-fancybox="gallery"
+                >
+                  <img src="${img.imgUrl}" alt="Image" class="img-fluid" />
+                  <div class="photo-text-more">
+                    <span class="icon icon-search"></span>
+                  </div>
+                </a>
+              </div>`);
   });
 });
