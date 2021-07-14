7f868812821d71a80599ec3ab930370c57bdfba6
rubenshibu <rubenshibu@gmail.com>
2021-07-14 23:11:25 +0530

    :lipstick:

--- js/jquery-ui.js
+++ js/jquery-ui.js
@@ -136,10 +136,6 @@ $.widget = function( name, base, prototype ) {
 		widgetFullName: fullName
 	} );
 
-	// If this widget is being redefined then we need to find all widgets that
-	// are inheriting from it and redefine all of them so that they inherit from
-	// the new version of this widget. We're essentially trying to replace one
-	// level in the prototype chain.
 	if ( existingConstructor ) {
 		$.each( existingConstructor._childConstructors, function( i, child ) {
 			var childPrototype = child.prototype;
